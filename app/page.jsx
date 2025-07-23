ce81914ceee0aa0d5ad284d9a8372a34c1057e72
arifbillah0156 <arifbillah0156@gmail.com>
2025-07-23 11:09:40 +0600

    edit54adf456aszbfe

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,21 @@
 import HomeBanners from "@/components/Home/HomeBanners";
-// import HomeSection10 from "@/components/Home/HomeSection10";
 import SmallBanners from "@/components/Home/SmallBanners";
-// import HomeSection3 from "@/components/Home/HomeSection3";
 import HomeSection4 from "@/components/Home/HomeSection4";
 import HomeSection5 from "@/components/Home/HomeSection5";
 import HomeSection6 from "@/components/Home/HomeSection6";
 import HomeSection7 from "@/components/Home/HomeSection7";
 import HomeSection8 from "@/components/Home/HomeSection8";
-// import HomeSection9 from "@/components/Home/HomeSection9";
 
 export default function Home() {
   return (
     <div>
       <HomeBanners />
       <SmallBanners />
-      {/* <HomeSection3 /> */}
       <HomeSection4 />
       <HomeSection5 />
       <HomeSection6 />
       <HomeSection7 />
       <HomeSection8 />
-      {/* <HomeSection9 /> */}
-      {/* <HomeSection10 /> */}
     </div>
   );
 }
--- components/Home/HomeBanners.jsx
+++ components/Home/HomeBanners.jsx
@@ -1,4 +1,3 @@
-// components/FullSlider.tsx
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -6,7 +5,6 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-// import Link from "next/link";
 
 const images = [
   {
@@ -20,11 +18,7 @@ const images = [
   {
     src: "/Banner3.jpg",
     alt: "DARUL AZHAR MODEL MADRASAH"
-  },
-  // {
-  //   src: "https://darulazharbd.com/wp-content/uploads/2024/12/225-2000x700-1.jpg",
-  //   alt: "EXAMINATION"
-  // }
+  }
 ];
 
 export default function FullSlider() {
@@ -41,14 +35,7 @@ export default function FullSlider() {
         {images.map((img, idx) => (
           <SwiperSlide key={idx}>
             <div className="w-full h-full relative">
-              {/* <Link
-                href={img.src}
-                passHref
-                rel="noopener noreferrer"
-                className="absolute text-lg md:text-2xl left-4 md:left-[1%] top-1 md:top-4 text-white md:p-1 md:px-2 md:rounded-md md:border md:border-white "
-              >
-                <i className="fa-solid fa-expand"></i>
-              </Link> */}
+
               <img
                 src={img.src}
                 alt={img.alt}
